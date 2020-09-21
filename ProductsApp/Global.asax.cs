02a1c4453149e2de9a7a7cb610e5b848a5c5aad2
Simuzeche Kaluwa <Skaluwa@nitel.me>
2020-09-21 10:07:56 +0200

    Add project files.

---
+++ ProductsApp/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using RestBus.RabbitMQ;
+using RestBus.RabbitMQ.Subscription;
+using RestBus.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ProductsApp
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+
+        RestBusHost restbusHost = null; //Make this a field in the class
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+
+
+            //*** Start RestBus subscriber/host **//
+
+
+            var amqpUrl = "amqp://localhost:5672"; //AMQP URI for RabbitMQ server
+            var serviceName = "products"; //Uniquely identifies this service
+
+            var msgMapper = new BasicMessageMapper(amqpUrl, serviceName);
+            var subscriber = new RestBusSubscriber(msgMapper);
+            restbusHost = new RestBusHost(subscriber, GlobalConfiguration.Configuration);
+            restbusHost.Start();
+
+
+            //****//
+        }
+
+        protected void Application_End()
+        {
+            if (restbusHost != null)
+                restbusHost.Dispose();
+        }
+    }
+}
---
+++ ProductsConsoleUI/Program.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using RestBus.Client;
+using RestBus.RabbitMQ;
+using RestBus.RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var amqpUrl = "amqp://localhost:5672"; //AMQP URL for RabbitMQ installation
+            var serviceName = "products"; //The unique identifier for the target service
+
+            var msgMapper = new BasicMessageMapper(amqpUrl, serviceName);
+
+            RestBusClient client = new RestBusClient(msgMapper);
+
+            RequestOptions requestOptions = null;
+
+            /* 
+            * //Uncomment this section to get a response in JSON format
+            * */
+            requestOptions = new RequestOptions();
+           requestOptions.Headers.Add("Accept", "application/json");
+            
+
+
+            //Send Request
+            var uri = "api/products"; //Substitute "hello/random" for the ServiceStack example
+            var response = client.GetAsync(uri, requestOptions).Result;
+            //NOTE: You can use 'var response = await client.GetAsync(uri, requestOptions);' in your code
+
+
+            var res = response.Content.ReadAsStringAsync().Result;
+
+
+            // var result = JsonConvert.DeserializeObject<ProductModel>(res);
+
+
+
+
+            dynamic json = JsonConvert.DeserializeObject(res);
+
+            //Display response
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(res);
+
+            //Dispose client
+            client.Dispose();
+
+        }
+    }
+}
